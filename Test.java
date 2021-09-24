13ce52bfabd7ad44b4a56b20c194def3872c5208
Michael MacDonald <michael2macdonald@gmail.com>
2021-09-24 12:13:55 -0700

    Got it working and changed it to open file

--- Test.java
+++ Test.java
@@ -1,35 +1,19 @@
-import java.awt.BorderLayout;
-import java.awt.FlowLayout;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
+import javax.swing.*;
+import java.awt.*;
 import java.io.File;
-import javax.swing.JButton;
-import javax.swing.JFileChooser;
-import javax.swing.JFrame;
-import javax.swing.SwingUtilities;
-import javax.swing.UIManager;
-import javax.swing.UnsupportedLookAndFeelException;
  
 /**
 * Java program to demonstrate how to use JFileChooser for showing open file
 * dialog and save file dialog in Java application.
  */
 public class Test extends JFrame {
- 
-    private final JButton upload = new JButton("Upload");
-    private final JButton save = new JButton("Save");
-   
- 
+
+    private final JButton upload = new JButton("open");
+
     public Test() {
-        super("JFileChooser Example - Open Save File Dialong in Java");
+        super("JFileChooser Example - Open File Dialog in Java");
         setLayout(new FlowLayout());
-        upload.addActionListener(new ActionListener() {
- 
-            @Override
-            public void actionPerformed(ActionEvent arg0) {
-                showSelectFileDialong();
-            }
-        });
+        upload.addActionListener((event) -> showOpenFileDialog());
         getContentPane().add(upload);
         setSize(300, 100);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -37,33 +21,18 @@ public class Test extends JFrame {
     }
  
     public static void main(String[] args) {
-        try {
-            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
-        } catch (ClassNotFoundException 
-                   | InstantiationException
-                   | IllegalAccessException 
-                   | UnsupportedLookAndFeelException e) {
-        }
- 
-        SwingUtilities.invokeLater(new Runnable() {
- 
-            @Override
-            public void run() {
-                Test t = new Test();
-            }
-        });
+        new Test();
     }
  
-    private void showSelectFileDialong() {
+    private void showOpenFileDialog() {
         JFileChooser fileChooser = new JFileChooser();
-        fileChooser.setDialogTitle("Choose a File to upload");
+        fileChooser.setDialogTitle("Choose a File to open");
  
         // pass reference of your JFrame here
         int response = fileChooser.showSaveDialog(this);
         if (response == JFileChooser.APPROVE_OPTION) {
             File selectedFile = fileChooser.getSelectedFile();
-            System.out.println("Save as file: " 
-                      + selectedFile.getAbsolutePath());
+            System.out.println("Open file: " + selectedFile.getAbsolutePath());
         }
     }
  
